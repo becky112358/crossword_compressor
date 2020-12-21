cc41e0a1cc504572abdea958f2a353acbb512653
becky112358 <8b2@protonmail.com>
2020-12-21 20:38:43 +0300

    Add a small test where it is visible exactly what is going on.

--- src/tests_combinations.rs
+++ src/tests_combinations.rs
@@ -23,4 +23,16 @@ mod tests {
             }
         }
     }
+
+    #[test]
+    fn test_get_combinations_3() {
+        let combinations = get_combinations(3);
+
+        assert!(combinations.contains(&vec![0,1,2]));
+        assert!(combinations.contains(&vec![0,2,1]));
+        assert!(combinations.contains(&vec![1,0,2]));
+        assert!(combinations.contains(&vec![1,2,0]));
+        assert!(combinations.contains(&vec![2,0,1]));
+        assert!(combinations.contains(&vec![2,1,0]));
+    }
 }
