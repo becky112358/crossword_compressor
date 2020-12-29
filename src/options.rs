dd0df76b9f72560f4332b804ecd0ce215cb74354
becky112358 <8b2@protonmail.com>
2020-12-29 11:40:28 +0300

    Reduce more duplication.

--- src/options.rs
+++ src/options.rs
@@ -80,7 +80,6 @@ fn get_position_start_end(
     return (position_start, position_end);
 }
 
-// todo get rid of duplication
 fn check_no_same_direction_overlaps(
     position_start: &[i32; 2],
     position_end: &[i32; 2],
@@ -89,42 +88,22 @@ fn check_no_same_direction_overlaps(
 
     let mut clear = true;
 
-    let mut start = position_start.clone();
-    let mut end = position_end.clone();
+    let z = match direction { Direction::Across => X, Direction::Down => Y, };
 
-    match direction {
-        Direction::Across => {
-            start[X] -= 1;
-            end[X] += 1;
-            clear = clear && check_row_clear(start[Y], X, &start, &end, crossword);
-            start[X] += 1;
-            end[X] -= 1;
-            start[Y] += 1;
-            end[Y] += 1;
-            clear = clear && check_row_clear(start[Y], X, &start, &end, crossword);
-            start[Y] -= 2;
-            end[Y] -= 2;
-            clear = clear && check_row_clear(start[Y], X, &start, &end, crossword);
-        }
-        Direction::Down => {
-            start[Y] -= 1;
-            end[Y] += 1;
-            clear = clear && check_row_clear(start[X], Y, &start, &end, crossword);
-            start[Y] += 1;
-            end[Y] -= 1;
-            start[X] += 1;
-            end[X] += 1;
-            clear = clear && check_row_clear(start[X], Y, &start, &end, crossword);
-            start[X] -= 2;
-            end[X] -= 2;
-            clear = clear && check_row_clear(start[X], Y, &start, &end, crossword);
-        }
-    }
+    let start_z = position_start[z];
+    let end_z = position_end[z];
+    let row = position_start[(z + 1) % 2];
+
+    clear = clear && check_row_clear(z, row, start_z-1, end_z+1, crossword);
+
+    clear = clear && check_row_clear(z, row+1, start_z, end_z, crossword);
+
+    clear = clear && check_row_clear(z, row-1, start_z, end_z, crossword);
 
     return clear;
 }
 
-fn check_row_clear(row: i32, z: usize, start: &[i32; 2], end: &[i32; 2], crossword: &Crossword) -> bool {
+fn check_row_clear(z: usize, row: i32, start: i32, end: i32, crossword: &Crossword) -> bool {
     let mut clear = true;
 
     let not_z = (z + 1) % 2;
@@ -132,10 +111,10 @@ fn check_row_clear(row: i32, z: usize, start: &[i32; 2], end: &[i32; 2], crosswo
     for word in &crossword.words {
         if let Some(position) = word.position {
             if position[not_z] == row {
-                if position[z] <= start[z] && position[z] + word.word.len() as i32 >= start[z] {
+                if position[z] <= start && position[z] + word.word.len() as i32 >= start {
                     clear = false;
                     break;
-                } else if position[z] >= start[z] && position[z] <= end[z] {
+                } else if position[z] >= start && position[z] <= end {
                     clear = false;
                     break;
                 }
