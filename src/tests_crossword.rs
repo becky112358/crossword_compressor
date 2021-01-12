09a79f311f2b3a8989af3ad9eb627299e0ffea16
becky112358 <8b2@protonmail.com>
2021-01-12 20:13:16 +0300

    Add a test for crossable_letters.

--- src/tests_crossword.rs
+++ src/tests_crossword.rs
@@ -13,5 +13,30 @@ mod tests {
         assert_eq!(Direction::Down, Direction::Across.change());
         assert_eq!(Direction::Across, Direction::Down.change());
     }
+
+    #[test]
+    fn test_crossword_crossable_letters() {
+        let crossword = Crossword {
+            words: vec![WordCross {word: "alpha",
+                                   cross: Some(CrossData {position: [5, 6], direction: Direction::Down, order: 1})},
+                        WordCross {word: "bravo",
+                                   cross: Some(CrossData {position: [3, 6], direction: Direction::Across, order: 0})},
+                       ]};
+
+        let crossable_letters = crossword.crossable_letters();
+
+        assert_eq!(vec![('a', [5, 6], Direction::Across),
+                        ('l', [5, 7], Direction::Across),
+                        ('p', [5, 8], Direction::Across),
+                        ('h', [5, 9], Direction::Across),
+                        ('a', [5, 10], Direction::Across),
+                        ('b', [3, 6], Direction::Down),
+                        ('r', [4, 6], Direction::Down),
+                        ('a', [5, 6], Direction::Down),
+                        ('v', [6, 6], Direction::Down),
+                        ('o', [7, 6], Direction::Down),
+                       ],
+                   crossable_letters);
+    }
 }
 
